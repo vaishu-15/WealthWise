d15fa152b2e30b2ef10df6063f7f9a787a0e0e45
vaishu-15 <vaishali15yadav@gmail.com>
2023-08-22 16:59:21 +0530

    all files inserted successfully

---
+++ initfirebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJXG2YESVybttVqp_M0slrjP0PTngerXk",
+  authDomain: "wealthwave-a1e74.firebaseapp.com",
+  projectId: "wealthwave-a1e74",
+  storageBucket: "wealthwave-a1e74.appspot.com",
+  messagingSenderId: "699246919866",
+  appId: "1:699246919866:web:812a32ef01d8b9698a8283",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("INIT CALLED", app);
